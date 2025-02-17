013ec01a2b82e4079c1e8ecbf8c732ea2cbed671
Laura Gomes <lauragomes.ti@gmail.com>
2025-02-17 17:40:44 -0300

    tramento para alguns erros

--- src/main/java/com/f1rst/registryapi/infra/exception/AgencyException.java
+++
@@ -1,7 +0,0 @@
-package com.f1rst.registryapi.infra.exception;
-
-public class AgencyException extends RuntimeException {
-    public AgencyException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/f1rst/registryapi/infra/exception/GlobalException.java
+++ src/main/java/com/f1rst/registryapi/infra/exception/GlobalException.java
@@ -1,7 +1,11 @@
 package com.f1rst.registryapi.infra.exception;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,8 +14,8 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class GlobalException {
 
-   @ExceptionHandler(AgencyException.class)
-    public ResponseEntity<ResponseError> featureException(AgencyException ex){
+   @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity<ResponseError> featureException(ValidacaoException ex){
 
        ResponseError response = new ResponseError(
                ex.getMessage(),
@@ -23,7 +27,7 @@ public class GlobalException {
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ResponseError> tratarException(Exception ex){
+    public ResponseEntity<ResponseError> treatException(Exception ex){
 
         ResponseError response = new ResponseError(
                 ex.getMessage(),
@@ -31,5 +35,34 @@ public class GlobalException {
                 LocalDateTime.now());
 
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity treatErro404() {
+
+       return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity treatErro400(MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors();
+        return ResponseEntity.badRequest().body(erros.stream().map(DataValidation::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity treatErro400(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(ValidacaoException.class)
+    public ResponseEntity treatErroRegraDeNegocio(ValidacaoException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+
+    private record DataValidation(String field, String mensage) {
+        public DataValidation(FieldError erro) {
+            this(erro.getField(), erro.getDefaultMessage());
+        }
     }
 }
